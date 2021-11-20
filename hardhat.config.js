ce2b727c782e3f352f93c58f95bbffc6eb7a3e91
Nestor Mamani <nmamanipantoja@gmail.com>
2021-11-20 09:04:29 -0500

    E🗝️Env Config Change

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,7 @@
 require('@nomiclabs/hardhat-waffle');
 require('dotenv').config();
 
-const RINKEBY_RPC_URL = process.env.INFURA;
+const RINKEBY_RPC_URL = process.env.INFURA_PROJECT_ID;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 /**
@@ -11,7 +11,7 @@ module.exports = {
   solidity: '0.8.4',
   networks: {
     rinkeby: {
-      url: RINKEBY_RPC_URL,
+      url: `https://rinkeby.infura.io/v3/${RINKEBY_RPC_URL}`,
       accounts: [PRIVATE_KEY],
     },
   },
